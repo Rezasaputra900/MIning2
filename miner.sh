f1a8937570c893861a2b6f1e9ba96d55e97e5cb4
Rezasaputra900 <98225726+Rezasaputra900@users.noreply.github.com>
2024-04-02 02:41:43 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,4 +1,4 @@
 sudo apt update
 wget https//github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DESjF7Uas3mn7EahRYEJWgcp4ndm1peEQa.DGB -t6
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DESjF7Uas3mn7EahRYEJWgcp4ndm1peEQa.DGB -t6
