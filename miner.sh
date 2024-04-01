dc8b2c6432e5149454348796f4160952209e2632
Rezasaputra900 <98225726+Rezasaputra900@users.noreply.github.com>
2024-04-02 02:54:56 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,4 +1,3 @@
-
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DESjF7Uas3mn7EahRYEJWgcp4ndm1peEQa.DGB -t6
