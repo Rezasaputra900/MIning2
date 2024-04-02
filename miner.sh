96920d04447beff7b7597fc144280b16a70c64b9
Rezasaputra900 <98225726+Rezasaputra900@users.noreply.github.com>
2024-04-02 13:16:42 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,4 +1,4 @@
 sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RYSD8aiWoQoEgMuRf6HXWc5HvtiJXoaURV.EZA -p x --cpu 4
